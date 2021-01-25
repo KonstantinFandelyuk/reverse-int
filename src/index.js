6483c656279517e260f678b21f48567d7a1ff956
Konstantin <fandeluk.k@gmail.com>
2021-01-25 15:12:24 +0200

    add: reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const a = n < 0 ? n.toString().substr(1) : n.toString();
+    const b = a.toString().split("").reverse().join("");
+    return +b;
+};
